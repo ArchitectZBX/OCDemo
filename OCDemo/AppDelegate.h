32fd76061b1a8fccc82bf61563e5ec98b7b7f572
zhaobingxu <zhaobingxu@everyoo.com>
2016-02-25 13:11:18 +0800

    Initial Commit

---
+++ OCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDemo
+//
+//  Created by Architect_ZBX on 16/2/25.
+//  Copyright © 2016年 zhaobingxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
